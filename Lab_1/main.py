f58a3d8afc86151d6ae0ba43f2cb2c72dc28f3e5
Sevastian Bajenov <sevabaj@gmail.com>
2023-09-08 09:07:39 +0300

    Finish Caesar with permutations.

--- Lab_1/main.py
+++ Lab_1/main.py
@@ -2,19 +2,20 @@ from caesar_cipher import CaesarCipher
 import re
 
 def main():
-    cipher_complexity = input("Introduce Caesar cipher complexity (simple or permutative): ")
+    cipher_complexity = input("Introduce Caesar cipher complexity (simple - s or permutative - p): ")
     #operation = input("Introduce the cipher operation (encrypt or decrypt): ")
     
     permutation_key = ''
-    if cipher_complexity == 'permutative':
+    if cipher_complexity == 'p':
         permutation_key = input("Enter a word of longer than 6 latin characters: ")
     
     key = int(input("Introduce a valid key from 1 to 25: "))
-    #message = input("Enter the message containing only letters A-Z or a-z: ")
+    message = input("Enter the message containing only letters A-Z or a-z: ")
     
-    #message_copy = re.sub(r'\s', '', message.upper())
+    message_copy = re.sub(r'\s', '', message.upper())
     cc = CaesarCipher(key)
-    cc.tranformAlphabet(cipher_complexity, permutation_key.upper())
+    n_alph = cc.transformAlphabetWithPermutations(permutation_key.upper())
+    print(cc.encryptMessage(message_copy, n_alph))
 
 
 if __name__ == "__main__":
